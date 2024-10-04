5caf404671a82d8ab09fbaedc183cf085e666ac6
02Vivek <guptayvivek.02@gmail.com>
2024-10-04 11:38:47 +0530

    modify api-client

--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "fc8ed4b2478c41f282002e2b15c408b8",
+    key: "add_your_api_key",
   },
 });
